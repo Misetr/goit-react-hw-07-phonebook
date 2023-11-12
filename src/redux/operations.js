bc01825a557eb611a76d66978e53ce75626efdd4
Misetr <135700846+Misetr@users.noreply.github.com>
2023-11-12 21:11:31 +0200

    fix code

---
+++ src/redux/operations.js
@@ -0,0 +1,42 @@
+
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://654fa9bd358230d8f0cd987c.mockapi.io/contacts/';
+
+export const fetchContactsThunk = createAsyncThunk(
+  'fetchContacts',
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axios.get('contacts');
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContactThunk = createAsyncThunk(
+  'addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const data = await axios.post('contacts', contact);
+      console.log(data.data);
+      return data.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Contact is exist');
+    }
+  }
+);
+
+export const deleteContactThunk = createAsyncThunk(
+  'deleteContact',
+  async (id, thunkAPI) => {
+    try {
+      await axios.delete(`contacts/${id}`);
+      return id;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message);
+    }
+  }
+);
